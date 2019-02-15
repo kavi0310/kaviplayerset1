9fc3f9f9d23a466f53db9c215b6bd47e1fbb842b
kavi0310 <46838903+kavi0310@users.noreply.github.com>
2019-02-15 15:59:32 +0530

    Create play 1.c

---
+++ play 1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+   char a[100];
+   int i,c=0;
+   scanf("%s",&a);
+   for(i=0;a[i]!='\0';i++)
+   {
+       c++;
+   }
+   for(i=c-1;i>=0;i--)
+   {
+       printf("%c",a[i]);
+   }
+   
+}
